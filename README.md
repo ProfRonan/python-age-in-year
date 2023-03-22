687829f05974a21d356ce4dd115a9b0c5756b77d
Ronan Soares <ronan.soares@gmail.com>
2023-03-22 08:39:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Idade no Ano
+
+O script principal de vocês deve estar no arquivo `main.py`.
+
+## 📝 Instruções 📝
+
+No `main.py`, escreva instruções para receber os valores abaixo na seguinte ordem:
+
+1. receber do usuário um número que é o ano atual.
+2. receber do usuário sua idade atual.
+3. receber do usuário outro número representando outro ano.
+4. receber do usuário o seu nome.
+
+A ordem de recebimento dos dados é importante para o funcionamento correto dos testes automáticos.
+
+O programa deve fazer os cálculos para imprimir na tela a frase `Fulano, no ano de XXXX você terá YYYY anos`, com os valores corretos.
+
+## 🧑‍💻 Exemplo de Execução 🧑‍💻
+
+Usuário digita:
+
+```
+2010
+15
+2023
+Fulano
+```
+
+O programa deve imprimir na tela:
+
+```
+Fulano, no ano de 2023 você terá 28 anos
+```
+
+## ❗ IMPORTANTE ❗
+
+Veja as instruções sobre como proceder com o exercício no link [Instruções Gerais Para os Exercícios](https://github.com/ProfRonan/python-exercise-instructions).
---
+++ test/test_main.py
@@ -0,0 +1,76 @@
+"""Esse arquivo testa o arquivo main.py"""
+
+import importlib  # para importar o módulo main dinamicamente
+import io  # para capturar a saída do print
+import sys  # para restaurar o stdout padrão e remover o módulo main do cache
+import unittest  # para criar o caso de teste
+from unittest.mock import patch  # para simular a entrada do usuário
+
+
+class TestMain(unittest.TestCase):
+    """Classe que testa o arquivo main.py"""
+
+    def setUp(self):
+        """
+        Inicializa o ambiente de teste removendo o módulo principal do cache.
+        Isso é necessário para ser capaz de importá-lo novamente em cada teste.
+        """
+        sys.modules.pop("main", None)
+
+    @patch("builtins.input", side_effect=["2010", "15", "2023", "Fulano"])
+    def test_idade_2010_15_2023_Fulano(self, _mock_input):
+        """
+        Testa se o programa imprime "Fulano, no ano de 2023 você terá 28 anos"
+        quando o usuário digita 2010, 15, 2023, Fulano
+        """
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn(
+            "Fulano, no ano de 2023 você terá 28 anos",
+            captured_output.getvalue().strip(),
+        )
+
+    @patch("builtins.input", side_effect=["0", "0", "0", "Beltrano"])
+    def test_idade_0_0_0_Beltrano(self, _mock_input):
+        """
+        Testa se o programa imprime "Beltrano, no ano de 0 você terá 0 anos"
+        quando o usuário digita 0, 0, 0, Beltrano
+        """
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn(
+            "Beltrano, no ano de 0 você terá 0 anos", captured_output.getvalue().strip()
+        )
+
+    @patch("builtins.input", side_effect=["2000", "10", "1990", "Ciclano"])
+    def test_idade_2000_10_1990_Ciclano(self, _mock_input):
+        """
+        Testa se o programa imprime "Ciclano, no ano de 1990 você terá 0 anos"
+        quando o usuário digita 2000, 10, 1990, Ciclano
+        """
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn(
+            "Ciclano, no ano de 1990 você terá 0 anos",
+            captured_output.getvalue().strip(),
+        )
+
+    @patch("builtins.input", side_effect=["2001", "0", "2010", "Alice"])
+    def test_idade_2001_0_2010_Alice(self, _mock_input):
+        """
+        Testa se o programa imprime "Alice, no ano de 2010 você terá 9 anos"
+        quando o usuário digita 2001, 0, 2010, Alice
+        """
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn(
+            "Alice, no ano de 2010 você terá 9 anos", captured_output.getvalue().strip()
+        )
